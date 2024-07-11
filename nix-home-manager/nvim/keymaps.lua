085a4603bc81f3e0f9e0424dc5029634e9a55bf7
JBrough <josiahbrough@proton.me>
2024-07-12 09:26:26 +1200

    Add copy and paste remaps to nvim config

--- nix-home-manager/nvim/keymaps.lua
+++ nix-home-manager/nvim/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Screen centred half page jump 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Screen centred jump to previous search term" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Screen centred jump to next search term" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set({ "n", "v", "i" }, "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
@@ -16,6 +17,14 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- visual mode selection movement
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- pasting
+vim.keymap.set("x", "<leader>p", [["_d"*P]], { desc = "Paste and replace selection from clipboard" })
+vim.keymap.set("n", "<leader>p", [["*p]], { desc = "Paste from system clipboard" })
+vim.keymap.set("n", "<leader>P", [["*P]], { desc = "Paste from system clipboard" })
+-- copying
+vim.keymap.set({ "n", "v" }, "<M-c>", [["*y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["*Y]], { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]], { desc = "Yank to system clipboard" })
 
 --[[ NvimTree ]]
 vim.keymap.set("n", "<leader>t", "<cmd>Neotree toggle<CR>", { desc = "Open file browser" })
